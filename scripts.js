1ceb240f15acba4984a45cefc3ca5f26b71c82e4
markfilipwsu <mark.filip@wsu.edu>
2022-11-13 22:53:11 +0000

    Updated matching CSS and JavaScript

--- scripts.js
+++ scripts.js
@@ -1,4 +1,86 @@
 /* Js code for memory card game - Tp-8-js */
+
+// set up card tracker
+cardsClicked = 0;
+
+
+// make sure the card hasnt been removed
+function cardClicked(what) {
+  if ( !what.classList.contains("matched") ) {
+    // its already clicked act appropriately
+    what.classList.remove("clicked");
+    cardsClicked--;
+  } else {
+    // its not already clicked
+     what.classList.add("clicked");
+    cardsClicked++;
+    
+    if (cardsClicked == 2) {
+      // compare card values
+      cardCompare();
+    }
+  }
+}
+
+function cardCompare() {
+   
+    clickedCards = document.querySelectorAll(".clicked"); // collection of cards
+
+  // first clicked element is clickedCards[0]
+  // second clicked element is clickedCards[1]
+  
+  matched = false; //track if the cards matched 
+  
+  if (clickedCards[0].classList.contains("pic1") && clickedCards[1].classList.contains("pic1") ) {
+  matched = true; // they matched pic1
+    
+  } else if (clickedCards[0].classList.contains("pic2") && clickedCards[1].classList.contains("pic2") ) {
+  matched = true; // they matched pic2
+}
+  
+  if (matched) {
+    // hide cards 
+    removeCards(clickedCards[0], clickedCards[1]);
+    
+    
+  } else {
+    // unflip cards 
+    unflipCards(clickedCards[0], clickedCards[1]);    
+    
+  }
+  
+}
+
+
+
+function removeCards(cardA, cardB) {
+  
+  
+  pause = setTimeout(function() {
+  cardA.classList.remove("clicked");
+  cardB.classList.remove("clicked");
+  
+  cardA.classList.add("matched");
+  cardB.classList.add("matched");
+  
+  cardsClicked = 0;
+  }, 1000); 
+  
+}
+
+
+
+function unflipCards(cardA, cardB) {
+  
+  pause = setTimeout(function() {
+  cardA.classList.remove("clicked");
+  cardB.classList.remove("clicked");
+    
+  cardsClicked = 0;  
+  }, 1000);
+}
+
+// stuff to do when page loads
 window.onload = function() {
   
   cardList = document.querySelectorAll(".card"); // collection of cards
@@ -7,7 +89,7 @@ window.onload = function() {
   
   for (c = 0; c < cardCount; c++) {
     cardList[c].onclick = function() {
-      this.classList.toggle("clicked");
+      cardClicked(this);
     }
   }
   
