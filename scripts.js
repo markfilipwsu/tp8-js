873a2a980e2e435e6de199682766e3d9e17bc5f8
markfilipwsu <mark.filip@wsu.edu>
2022-11-13 21:09:32 +0000

    added patterns and basic clicks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Js code for memory card game - Tp-8-js */
+window.onload = function() {
+  
+  cardList = document.querySelectorAll(".card"); // collection of cards
+  
+  cardCount = cardList.length; // how many cards are on the table
+  
+  for (c = 0; c < cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+  
+  
+  
+  
+}
